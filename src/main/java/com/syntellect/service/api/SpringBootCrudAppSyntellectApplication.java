3c39d3878dca9d092bf40f43c49bfea49ca07915
Bindisha <indishaparmar01@gmail.com>
2023-08-29 22:43:23 +0530

    Initial workspace set up

---
+++ src/main/java/com/syntellect/service/api/SpringBootCrudAppSyntellectApplication.java
@@ -0,0 +1,13 @@
+package com.syntellect.service.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCrudAppSyntellectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCrudAppSyntellectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/syntellect/service/api/SpringBootCrudAppSyntellectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.syntellect.service.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudAppSyntellectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
